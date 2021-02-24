13783f115d87d988ad42d9af3f96c489287a8de3
tbutler4 <terrencebutler@Mac-mini.local>
2021-02-24 15:44:04 -0800

    fix(models) fixing models associations

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      //models.user.hasMany(models.watchlist)
+      models.user.hasMany(models.watchList)
     }
     
   // validPassword
--- models/watchlist.js
+++ models/watchlist.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      //models.watchlist.belongsTo(models.user)
+      models.watchList.belongsTo(models.user)
     }
   };
   watchList.init({
