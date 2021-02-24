1fcaeeca6582376ba93ad9342e8b02eb5202c1e0
didin21 <tanashikovadina@gmail.com>
2021-02-24 10:10:01 -0800

    search function

--- middleware/requests.js
+++ middleware/requests.js
@@ -9,6 +9,7 @@ const requests = {
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
   fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  searchMovie: `/search/movie?api_key=${API_KEY}&query=`
 }
 
 module.exports = requests;
--- server.js
+++ server.js
@@ -1,65 +1,87 @@
-const requests = require('./middleware/requests');
-const axios = require('./middleware/axios');
-const trending = require('./middleware/trending');
-
-require('dotenv').config();
-const express = require('express');
-const layouts = require('express-ejs-layouts');
-const session = require('express-session');
-const flash = require("connect-flash")
-const passport = require('./config/ppConfig');
-const isLoggedIn = require('./middleware/isLoggedIn')
+const requests = require("./middleware/requests");
+const axios = require("./middleware/axios");
+const trending = require("./middleware/trending");
+require("dotenv").config();
+const express = require("express");
+const layouts = require("express-ejs-layouts");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("./config/ppConfig");
+const isLoggedIn = require("./middleware/isLoggedIn");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(require('morgan')('dev'));
+app.use(require("morgan")("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(layouts);
 
-app.use(session({
-  // a string used to generate a unique 
-  // session ID cookie to share with the browser
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true 
-}))
+app.use(
+  session({
+    // a string used to generate a unique
+    // session ID cookie to share with the browser
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // the following two lines must appear after configuring the session
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // FLASH
-app.use(flash())
+app.use(flash());
 // adds a method to the req object for universal access
 
 // Set up local variables (data that's accessible from anywhere in the app)
 app.use((req, res, next) => {
-  // before every route is loaded, attach flash messages and the 
+  // before every route is loaded, attach flash messages and the
   // current user to res.locals
-  res.locals.alerts = req.flash()
-  res.locals.currentUser = req.user
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
 
-  next()
-})
+  next();
+});
 // grabbing movie data
 
-
-app.get('/', async (req, res) => {
-  const grabData = await trending.fetchTrend()
-  res.render('index', {
-    myTrending: {grabData}
+app.get("/", async (req, res) => {
+  const grabData = await trending.fetchTrend();
+  res.render("index", {
+    myTrending: { grabData },
   });
 });
+let myMovieRes = []
+app.get("/:search", (req, res)=>{
+  let keyword  = req.query.title
+  axios.get(`${requests.searchMovie}${keyword}`)
+   .then((responseData)=>{
+     const searchData = responseData.data.results
+     searchData.forEach((movie)=>{ 
+       if(movie.poster_path!= null){
+          myMovieRes.push(movie)
+     } 
+  }) 
+  res.render("search", {responseData: myMovieRes})
+  myMovieRes = []
+  })
+})
 
-app.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile');
+
+app.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile");
 });
 
-app.use('/auth', require('./routes/auth'));
+app.use("/auth", require("./routes/auth"));
 
-var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
+var server = app.listen(process.env.PORT || 3000, () =>
+  console.log(
+    `🎧You're listening to the smooth sounds of port ${
+      process.env.PORT || 3000
+    }🎧`
+  )
+);
 
 module.exports = server;
