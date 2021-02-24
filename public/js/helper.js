6f545c769f1c0a3ef18a8545c08638abb024f6c9
tbutler4 <terrencebutler@Mac-mini.local>
2021-02-23 22:38:53 -0800

    enh(click function): added boilerplate pop up

--- public/js/helper.js
+++ public/js/helper.js
@@ -1,1 +1,18 @@
-function singleVideo(name){console.log("here", name)}
+function singleVideo(name){
+  var el = document.querySelector("#append");
+  var iframe = document.createElement('iframe');
+  movieTrailer( 'Oceans Eleven', { id: true } )
+    .then( response => {
+      if(document.querySelector("#iframe")){
+        let item = document.getElementById('iframe');
+        item.remove();
+      }else{                             
+        iframe.setAttribute("src", `https://www.youtube.com/embed/${response}`);
+        iframe.setAttribute("id", 'iframe');
+        iframe.style.width = "100%";
+        iframe.style.height = "400px";
+        el.appendChild(iframe)
+      }
+      // console.log( response ) 
+    })
+}
